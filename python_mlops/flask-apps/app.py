02e24c02e07d4b3c99b6aaa8f18ee44ad06a9a08
Boyd, Sorratat | Siri | DSAD <sorratat.boyd@rakuten.com>
2023-12-25 15:18:07 +0900

    add automation script and flask api

--- python_mlops/flask-apps/app.py
+++ python_mlops/flask-apps/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort
+from flask import Flask, abort, jsonify, request
 from transformers import pipeline
 from pydantic import BaseModel
 
@@ -21,10 +21,21 @@ def error():
     abort(500, "Oops! Something went wrong...")
     
     
-@app.post('/generate')
-def predict(body: Body):
-    results = generator(body.test, max_length=35, num_return_sequence=1)[0]
-    return results
+@app.route('/generate', methods=['POST'])
+def generate_text():
+    try:
+        request_data = request.get_json()
+        input_text = request_data.get('text', '')
+        if not input_text:
+            return jsonify({"error": "Please provide text input"}), 400
+
+        # Use the GPT-2 model to generate text
+        generated_text = generator(input_text, max_length=35, num_return_sequences=1)[0]['generated_text']
+
+        return jsonify({"input_text": input_text, "generated_text": generated_text})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
